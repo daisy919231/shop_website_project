926be25683940df88572b3bfe5cc831c44cc1f78
Daisy <shahzodaakhmedova91@gmail.com>
2024-08-06 23:37:01 +0300

    updates available

--- shop_website/urls.py
+++ shop_website/urls.py
@@ -10,7 +10,8 @@ urlpatterns=[
     path('product/<int:product_id>/place-order/', views.place_order, name='place_order'),
     path('add-product/', views.add_product, name='add_product'),
     path('delete-product/<int:product_id>/', views.delete_product, name='delete_product'),
-    path('edit-product/<int:product_id>/', views.edit_product, name='edit_product')
+    path('edit-product/<int:product_id>/', views.edit_product, name='edit_product'),
+    path()
 
     
 
--- shop_website/views.py
+++ shop_website/views.py
@@ -5,23 +5,51 @@ from shop_website.forms import CommentModelForm, OrderModelForm, ProductModelFor
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 # Create your views here.
 def product_list(request, category_id: Optional[int]=None ):
     categories=Category.objects.all().order_by('id')
+    search=request.GET.get('q')
+    filter_type=request.GET.get('filter', '')
     if category_id:
-        products=Product.objects.filter(category=category_id)
+        if filter_type == 'expensive':
+            products=Product.objects.filter(category=category_id).order_by('-price')
+        elif filter_type=='cheap':
+            products=Product.objects.filter(category=category_id).order_by('price')
+        elif filter_type == 'quality_products':
+             products=Product.objects.filter(Q(category=category_id) & Q(rating__gte=4)).order_by('-rating')
+        else:
+            products=Product.objects.filter(category=category_id)
+            
     else:
-        products = Product.objects.all()
+        if filter_type == 'expensive':
+            products=Product.objects.all().order_by('-price')
+        elif filter_type=='cheap':
+            products=Product.objects.all().order_by('price')
+        elif filter_type == 'quality_products':
+             products=Product.objects.filter(Q(rating__gte=4)).order_by('-rating')
+        else:
+            products = Product.objects.all()
+    if search:
+        products=products.filter(Q(name__icontains=search)| Q(comments__comment__icontains=search)).distinct()
     context={
     'products': products,
     'categories': categories } 
     return render (request, 'shop_website/home.html', context)
 
-def product_details (request, product_id):
+def product_details (request, product_id, category_id, displayed_product_id):
+    search=request.GET.get('q')
+    categories=Category.objects.all()
     product=Product.objects.get(id=product_id)
     comments=Comment.objects.filter(product=product_id, is_provided=True).order_by('-id')
+    if search:
+        comments=comments.filter(Q(comment__icontains=search))
+        # if not related to only one product, but for all products:
+        # comments=Comment.objects.filter(Q(comment__icontains=search))
     context={
         'product': product,
+        'comments': comments,
+        'categories': categories, 
         'comments': comments
         }
     return render (request, 'shop_website/detail.html', context)
@@ -179,5 +207,13 @@ def edit_product(request, product_id):
 
     return render (request, 'shop_website/edit-product.html', context)
 
+def related_products(request, category_id, displayed_product_id):
+    category=get_object_or_404(Category, id=category_id)
+    products=Product.objects(category=category).exclude(id=displayed_product_id)
+    context={
+        'category':category,
+        'products':products,
+    }
+    return render (request, 'shop_website/detail.html', context)
 
 
